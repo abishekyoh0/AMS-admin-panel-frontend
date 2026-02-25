a4a97fcdab91ff7ba4427676c64ed566191f8b66
Abishek <abish.yoho@gmail.com>
2026-02-25 12:41:03 +0530

    Correction

---
+++ src/components/ParkingManagement/parkingslots/Deleteprakingslot.tsx
@@ -0,0 +1,10 @@
+
+
+export default function Deleteprakingslot() {
+  return (
+    <div className="fixed inset-0 bg-black/60 backdrop-blur-sm flex items-center justify-center z-50 p-4">
+      <div className="w-full sm:w-[90%] md:w-[600px] lg:w-[700px] max-h-[90vh] overflow-y-auto bg-gradient-to-br from-[#0F172B] to-[#101828] border border-white/10 rounded-2xl p-5 sm:p-6 md:p-8 shadow-2xl relative ">
+      </div>
+    </div>
+  );
+}
